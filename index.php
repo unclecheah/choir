98ddb26f910a0298e7a44f7a6e778a28e0bc8d1d
Andrew Cheah <andrew@unclecheah.com>
2024-07-12 22:55:02 +0800

    1. background 2. sparkle

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
     <head>
         <title>CD Choir</title>
         <link rel="stylesheet" href="css/main.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/sparkle.css" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+        <script src="js/sparkle.js"></script>
 
         <script>
             window.onload = async function () {
-                $('main').css('opacity', 0); // Set initial opacity of main to 0
+                sparkle ();
                 $('main').animate({ opacity: 1 }, 2000); // Fading in HTML content
                 $('.webgl').animate({ opacity: 0}, 2000);
             };
@@ -16,7 +18,7 @@
 
     <body>
         <canvas class="webgl"></canvas>
-
+        <div id="starshine"><div class="template shine"></div></div>
         <main>
             <h1>This is HTML</h1>
         </main>
